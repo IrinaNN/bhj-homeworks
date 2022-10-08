115082cbd69b379987f167427da726408c87cf76
IrinaNN <irig_nazarova@mail.ru>
2022-10-08 19:23:41 +0300

    1.1 «Возможности JavaScript в браузере». Игра-кликер

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,15 @@
-
+const image = document.getElementById("cookie");
+function changeSize() {
+	return image.width = image.width === 250 ? 200 : 250;
+}
+const timer = document.getElementById("clicker__counter");
+let text = timer.textContent; 
+timer.textContent = "0"; 
+function game() {
+	console.log(timer.textContent++)
+}; 
+image.addEventListener("click", function() {
+	changeSize(); 
+	game();
+}
+);
