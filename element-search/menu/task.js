df3078780da370a6fb953ce44a8b93d7887c34a4
IrinaNN <irig_nazarova@mail.ru>
2022-10-08 22:53:21 +0300

    1.2 «Способы поиска нужного HTML-элемента». Навигационное меню.

---
+++ element-search/menu/task.js
@@ -0,0 +1,16 @@
+const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
+const hasSubMenu = menuLinks.filter(link => link.nextElementSibling !== null);
+hasSubMenu.forEach(function(link) {
+	link.onclick = function() {
+		let clickLink = link;
+		hasSubMenu.forEach(function(link) {
+			if (link === clickLink) {
+				link.nextElementSibling.classList.toggle('menu_active');
+			} 
+			else {
+				link.nextElementSibling.classList.remove('menu_active');
+			}
+		})
+		return false;
+  }
+});
